9a8d6cf5c30f52da7a0916ddacb7f244a171ac68
hosein mohammadi <hosein011dev@gmail.com>
2025-11-02 15:21:23 +0330

    NavBar UI

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+'use client'
+import React, { useState } from "react";
 
 export default function NavBar() {
-    return (
-        <section className="mx-auto mt-11 w-[90%] sm:w-[495px] h-[66px] bg-(--fill-gradient) rounded-(--border-radius)" style={{background: "var(--fill-gradient)",}}>
-            
-        </section>
-    )
+  const [active, setActive] = useState("Hero");
+
+  const navItems = ["Hero", "About Me", "Projects", "Contact"];
+
+  return (
+    <section
+      className="mx-auto mt-11 w-[95%] sm:w-[495px] h-[66px] rounded-(--border-radius) border border-(--stoke-color) py-5 px-3 sm:px-6"
+      style={{ background: "var(--fill-gradient)" }}
+    >
+      <ul className="flex justify-between text-(--link-text) text-[14px] sm:text-[16px]">
+        {navItems.map((item) => (
+          <li key={item} className="relative cursor-pointer"  onClick={() => setActive(item)}>
+
+            <span className={`transition-all duration-300 ${active === item ? "text-white" : "opacity-70 hover:opacity-100"}`}>
+              {item}
+            </span>
+
+            <span className={`absolute left-1/2 -translate-x-1/2 -bottom-5 w-2 h-2 rounded-full transition-all duration-300 ${ active === item ? "bg-white opacity-100 scale-100": "bg-white/70 opacity-0 scale-0 group-hover:scale-100"}`}/>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ export default function HomePage() {
   return (
     <>
       <header className="w-full h-screen">
-        <div className="absolute top-0 left-1/2 -translate-x-1/2 w-2/3  h-screen bg-[url('/image/grid-pattern.svg')] bg-contain bg-no-repeat z-0"></div>
+        <div className="absolute top-0 left-1/2 -translate-x-1/2 w-2/3  h-screen bg-[url('/image/grid-pattern.svg')] bg-cover bg-no-repeat z-0"></div>
         <div className="hidden sm:block absolute top-0 left-0 w-2/3 h-2/3 bg-[url('/image/spotlight.png')] bg-contain bg-no-repeat"></div>
         <div className="hidden sm:block absolute top-0 right-0 w-2/3 h-2/3 bg-[url('/image/spotlight.png')] bg-contain bg-no-repeat scale-x-[-1]"></div>
         <NavBar />
