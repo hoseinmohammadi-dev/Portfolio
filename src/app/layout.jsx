94571ac71138af460b65d18dc8ce798186df4f3c
hosein mohammadi <hosein011dev@gmail.com>
2025-11-08 00:02:03 +0330

    SEO

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,21 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "Hosein Mohammadi – Frontend Developer",
+  description:
+    "Portfolio website of Hosein Mohammadi, a passionate Frontend Developer. Explore projects, skills, and contact information.",
+  keywords: ["Hosein Mohammadi", "hoseinmdev", "Frontend Developer", "Web Developer" , "حسین محمدی" , "حسین محمدی فرانت اند" ,  "حسین محمدی دولوپر"],
+  authors: [{ name: "Hosein Mohammadi" }],
+  openGraph: {
+    title: "Hosein Mohammadi – Frontend Developer",
+    description:
+      "Portfolio website of Hosein Mohammadi, a passionate Frontend Developer. Explore projects, skills, and contact information.",
+    url: "https://hoseinmdev.ir",
+    siteName: "Hosein Mohammadi Portfolio",
+    type: "website",
+  }
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
