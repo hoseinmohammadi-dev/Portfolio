f4091ed001dfaefd8f6d562ceb955e262038d897
hosein mohammadi <hosein011dev@gmail.com>
2025-11-08 16:10:42 +0330

    add favIcon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -28,6 +28,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/image/profile.png" type="image/png" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
